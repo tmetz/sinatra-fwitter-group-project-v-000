fd6cc0e69b8dc4a50be555548ef593e806708126
Tammy Metz <tammy@tammymetz.com>
2018-12-30 05:50:36 -0500

    fix merge issue with users_controller.rb and rename tweet views to conform to RESTful convention

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,25 @@
 class UsersController < ApplicationController
-<<<<<<< HEAD
-
-  get '/signup' do
-    erb :'users/create_user'
-  end
-
-
-
-=======
-    get "/login" do
+    
+    get '/signup' do
+        erb :'users/create_user'
+    end
+    
+    et "/login" do
         erb :login
-      end
-    
-      post "/login" do
+    end
+        
+    post "/login" do
         user = User.find_by(:username => params[:username])
-            if user && user.authenticate(params[:password])
-                session[:user_id] = user.id
-                redirect "/tweets"
-            else
-                redirect "/failure"
-            end
-      end
-    
-      get "/failure" do
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect "/tweets"
+        else
+            redirect "/failure"
+        end
+    end
+        
+    get "/failure" do
         erb :failure
-      end
->>>>>>> 44f424c1435b0869a06c8ecbdc9a4312b970be1c
-
+    end
+        
 end
