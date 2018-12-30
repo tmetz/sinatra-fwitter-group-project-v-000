03e9f90777f9c2afc65d4f69ca4275e3a8fe8618
HarleyHarl <harleymteden@gmail.com>
2018-12-30 09:53:51 -0800

    users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,8 @@
 class UsersController < ApplicationController
-<<<<<<< HEAD
-
-  get '/signup' do
-    erb :'users/create_user'
-  end
-
-
-
-=======
     get "/login" do
         erb :login
       end
-    
+
       post "/login" do
         user = User.find_by(:username => params[:username])
             if user && user.authenticate(params[:password])
@@ -21,10 +12,9 @@ class UsersController < ApplicationController
                 redirect "/failure"
             end
       end
-    
+
       get "/failure" do
         erb :failure
       end
->>>>>>> 44f424c1435b0869a06c8ecbdc9a4312b970be1c
 
 end
