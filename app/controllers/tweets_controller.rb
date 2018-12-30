44f424c1435b0869a06c8ecbdc9a4312b970be1c
Tammy Metz <tammy@tammymetz.com>
2018-12-29 20:46:14 -0500

    set up several controller actions

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,17 @@
 class TweetsController < ApplicationController
 
+    get '/tweets/new' do
+        @user = User.find(session[:user_id])
+        erb :'/tweets/new'
+    end
 
+    get '/tweets' do
+        @tweets = Tweet.where(["user_id = ?", session[:user_id]])
+        erb :'/tweets/index'
+    end
+
+    post '/tweets' do
+        Tweet.create(params[:tweet])
+        redirect to ("/tweets")
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
-
+    get "/login" do
+        erb :login
+      end
+    
+      post "/login" do
+        user = User.find_by(:username => params[:username])
+            if user && user.authenticate(params[:password])
+                session[:user_id] = user.id
+                redirect "/tweets"
+            else
+                redirect "/failure"
+            end
+      end
+    
+      get "/failure" do
+        erb :failure
+      end
 
 end
