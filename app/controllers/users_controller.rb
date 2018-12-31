679f7c0a608974fe8cec00b974f58ac5e9302c9f
HarleyHarl <harleymteden@gmail.com>
2018-12-31 11:44:45 -0800

    changed /signup route to not let a logged in user view the signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,18 @@
 class UsersController < ApplicationController
 
     get '/signup' do
+      if logged_in?
+        redirect '/tweets'
+      else
         erb :'users/create_user'
+      end
     end
 
     post '/signup' do
       if !params[:username].empty? && !params[:email].empty? && !params[:password].empty?
         @user = User.create(username: params[:username], email: params[:email], password: params[:password])
         session[:user_id] = @user.id
-        #binding.pry
+        # binding.pry
         redirect "/tweets"
       else
         redirect "/signup"
