e0aa5d5c306c284ca822eb90371819ab189081b4
HarleyHarl <harleymteden@gmail.com>
2018-12-30 22:51:45 -0800

    built out failure view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
     end
 
     post '/signup' do
+      # binding.pry
       if !params[:username].empty? && !params[:email].empty? && !params[:password].empty?
         User.create(username: params[:username], email: params[:email], password: params[:password])
         redirect '/tweets'
