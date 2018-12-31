191a208abfd29d103314cec9f50ecff182e475ba
Tammy Metz <tammy@tammymetz.com>
2018-12-31 07:32:53 -0500

    create edit tweet action and view

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -27,4 +27,26 @@ class TweetsController < ApplicationController
             redirect to ("/tweets")
         end
     end
+
+    get '/tweets/:id/edit' do
+        if logged_in?
+            @tweet = Tweet.find_by_id(params[:id])
+            @user = User.find(session[:user_id])
+            erb :'/tweets/edit'
+        else
+            redirect to ("/login")
+        end
+    end
+
+    patch '/tweets/:id' do
+        if params[:tweet][:content] == ""
+            redirect to ("/tweets/#{params[:id]}/edit")
+        else
+            @tweet = Tweet.find_by_id(params[:id])
+            if @tweet.content != params[:tweet][:content]
+                @tweet.content = params[:tweet][:content]
+            end
+            redirect to ("/tweets")
+        end
+    end
 end
