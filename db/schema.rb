cf9b1bc8116583f2e0ee8dbd8b4fdae7e11762b1
Tammy Metz <tammy@tammymetz.com>
2018-12-31 07:04:32 -0500

    add migration to add foreign key user_id to tweets table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181231111001) do
+ActiveRecord::Schema.define(version: 20181231120132) do
 
   create_table "tweets", force: :cascade do |t|
-    t.text "content"
+    t.text    "content"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
