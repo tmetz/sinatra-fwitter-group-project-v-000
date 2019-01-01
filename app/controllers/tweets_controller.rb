e5e04dfc03fdf2176ba5048e6f9ca855c243d46f
Tammy Metz <tammy@tammymetz.com>
2019-01-01 08:01:10 -0500

    create view and controller action for show tweet page

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -28,6 +28,15 @@ class TweetsController < ApplicationController
         end
     end
 
+    get '/tweets/:id' do
+        if logged_in?
+            @tweet = Tweet.find_by_id(params[:id])
+            erb :'/tweets/show'
+        else
+            redirect to ("/login")
+        end
+    end
+
     get '/tweets/:id/edit' do
         if logged_in?
             @tweet = Tweet.find_by_id(params[:id])
